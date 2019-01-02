d898ed754295cfea9bcae63b62c4c96f9ec04c41
Nicolae Iancu <iancunicky@gmail.com>
2019-01-02 15:18:21 +0200

    v2

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -2,37 +2,35 @@
 require_once("config.php");
 
 class Database{
-	
+
 	public $connection;
-	
-	
+
+
 	public function __construct(){
 		$this->open_db_connection();
 	}
-	
+
 	public function open_db_connection(){
-	
-		//$this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-		
+
 		$this->connection = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-		
+
 		if($this->connection->connect_errno){
 			die("Database connection failed" . $this->connection->connect_error);
 		}
-		
+
 	}
-	
+
 	public function query($sql){
 		//$result = mysqli_query($this->connection, $sql);
 		$result = $this->connection->query($sql);
-		
+
 		$this->confirm_query($result);
-		
+
 		return $result;
 	}
 	private function confirm_query($result){
-		
-		
+
+
 		if(!$result){
 			die("Query failed" . $this->connection->error);
 		}
@@ -45,7 +43,7 @@ class Database{
 	public function the_insert_id(){
 		return $this->connection->insert_id;
 	}
-	
+
 }
 
 
@@ -55,7 +53,3 @@ $database = new Database();
 
 
 ?>
-	
-	
-
-
